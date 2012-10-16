c8557c526d017ef2a2d62c9335c81ee8be6e4c3a
Tang Qiao <tangqiao@fenbi.com>
2012-10-16 17:21:06 +0800

    ADD: add image view and show output to imageView.

--- AVFoundationDemo/ViewController.m
+++ AVFoundationDemo/ViewController.m
@@ -14,14 +14,25 @@
 @property (nonatomic, strong) AVCaptureDevice * videoDevice;
 @property (nonatomic, strong) AVCaptureDeviceInput * videoInput;
 @property (nonatomic, strong) AVCaptureVideoDataOutput * frameOutput;
+@property (nonatomic, strong) CIContext * context;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
 @implementation ViewController
 
+- (CIContext *)context {
+    if (_context == nil) {
+        _context = [CIContext contextWithOptions:nil];
+    }
+    return _context;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    debugMethod();
 
     // Session
     _session = [[AVCaptureSession alloc] init];
@@ -34,11 +45,11 @@
     // Output
     _frameOutput = [[AVCaptureVideoDataOutput alloc] init];
     _frameOutput.videoSettings = @{(id)kCVPixelBufferPixelFormatTypeKey : @(kCVPixelFormatType_32BGRA)};
+    [_frameOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
 
     [_session addInput:_videoInput];
     [_session addOutput:_frameOutput];
 
-    [_frameOutput setSampleBufferDelegate:self queue:dispatch_get_main_queue()];
     [_session startRunning];
 }
 
@@ -48,7 +59,20 @@
 }
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+    CVPixelBufferRef pb = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CIImage * ciImage = [CIImage imageWithCVPixelBuffer:pb];
 
+    CGImageRef ref = [self.context createCGImage:ciImage fromRect:ciImage.extent];
+    _imageView.image = [UIImage imageWithCGImage:ref scale:1.0 orientation:(UIImageOrientationRight)];
+    CFRelease(ref);
 }
 
+- (void)viewDidUnload {
+    [self setImageView:nil];
+    [super viewDidUnload];
+}
 @end
+
+
+
+
