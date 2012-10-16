f50169c58e03f7419df1d6b16705996f6a683255
Tang Qiao <tangqiao@fenbi.com>
2012-10-16 15:59:11 +0800

    Initial Commit

---
+++ AVFoundationDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AVFoundationDemo
+//
+//  Created by TangQiao on 12-10-16.
+//  Copyright (c) 2012年 TangQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AVFoundationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVFoundationDemo
+//
+//  Created by TangQiao on 12-10-16.
+//  Copyright (c) 2012年 TangQiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
